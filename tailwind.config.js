0287660b8f0fd060c818b6c7fc76f663e3eded40
Jeferson0Silva <jefersonsas12@gmail.com>
2025-02-05 11:46:47 -0300

    feat: carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       },
       spacing: {
         "custom-h": "calc(100vh - 4rem)",
-        "custom-1": "calc(100vh - 40vh)",
+        "custom-1": "calc(100vh - 10vh)",
         "custom-w": "calc(100vw - 30vw)",
       },
     },
